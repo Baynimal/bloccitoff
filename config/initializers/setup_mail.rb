eda752a994a7769bdfde0184faf4a747414015d2
Andrew Baynes <dru_baynes@hotmail.com>
2016-10-10 14:31:31 -0700

    adding users controller

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,8 +5,8 @@ if Rails.env.development? || Rails.env.production?
     port:         '2525',
     authentication: :plain,
     user_name:    ENV['SENDGRID_USERNAME'],
-    password:     ENV['SENDGRID_PASSWORD'],
+    password:      ENV['SENDGRID_PASSWORD'],
     domain:       "heroku.com",
     enable_starttls_auto: true
   }
-end 
+end
